c3c97995964cfe4438ed457707746464909c17cc
sugihara10969 <sugihara.10969@gmail.com>
2020-08-21 16:31:15 +0900

    modify cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://sugihara10969.github.io/My-HomeWork-Todo-App/'
+    origins 'http://localhost:3000', 'https://sugihara10969.github.io/'
 
     resource '*',
       headers: :any,
